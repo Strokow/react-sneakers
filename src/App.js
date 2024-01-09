acc4dd862d97ee8242fdab5a8c1ffa61f869147c
Strokow <aleksestroko@yandex.com>
2024-01-09 12:48:14 +0100

    0901 update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import React from 'react';
+
 function App() {
   return (
-    <div className="Wrapper">
-      <header>
-        <div className="headerLeft">
-        <svg />
-        <div className="headerInfo">
-          <h3> React Sneakers </h3>
-          <p>Магазин лучших кроссовок</p>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center" >
+        <div className="d-flex align-center">
+          <img width={80} height={80} src="/shoelogo.png" alt="Shoe Logo" />
+          <div>
+            <h3> React Sneakers </h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
         </div>
-        </div>
-        <ul className="headerRight=">
-          <li>
-            <svg />
-            <span>1002 руб.</span>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={30} height={30} src="/korblogo2.png" alt="Korb Logo" /> 
+            <span>1299 ₽</span>
           </li>
           <li>
-            <svg />
+            <img width={30} height={30} src="usericon.png" alt="User icon"  />
           </li>
         </ul>
       </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
     </div>
   );
 }
