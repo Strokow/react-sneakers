05d6f13e9861c6a959f379a7bc70b71465ac8055
Strokow <aleksestroko@yandex.com>
2024-01-24 10:47:59 +0100

    2401 update 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr =[
-{name: 'Мужские кроссовки Adidas Stan Smith', price: '7999 ₽'},
-/*{name: 'Мужские кроссовки Nike Jordan', price: '5599 ₽'},
-{name: 'Женские кроссовки Nike', price: '10000 ₽ ₽'},
-{name: 'Мужские кроссовки New Balance 574s', price: '7999 ₽'},*/
-]
+const arr = [
+  {
+    title: 'Мужские кроссовки Adidas Stan Smith',
+    price: '7999 ₽',
+    imageUrl: '/img/sneakers1.jpg',
+  },
+  {
+    title: 'Мужские кроссовки Nike Jordan',
+    price: '5599 ₽',
+    imageUrl: '/img/sneakers2.jpg',
+  },
+  {
+    title: 'Женские кроссовки Nike Pink',
+    price: '10000 ₽',
+    imageUrl: '/img/sneakers3.jpg',
+  },
+  {
+    title: 'Мужские кроссовки New Balance 574s',
+    price: '7999 ₽',
+    imageUrl: '/img/sneakers4.jpg',
+  },
+];
 
 function App() {
   return (
@@ -16,38 +32,19 @@ function App() {
       <Drawer />
       <Header />
       <div className="content p-40">
-  <div className="d-flex align-center justify-between mb-40">
-  <h1>Все кроссовки</h1>
-  <div className="search-block d-flex align-center">
-    <img width={14} height={14} src="/img/search.png" alt="Search"></img>
-    <input placeholder="Поиск"></input>
-
-  </div>
-  </div>
-  <div className="d-flex">
-    <Card 
-    title="Мужские кроссовки Adidas Stan Smith" 
-    price={'7999 ₽'} 
-    imageUrl="/img/sneakers1.jpg"
-    />
-    <Card 
-    title="Мужские кроссовки Nike Jordan" 
-    price={'5599 ₽'} 
-    imageUrl="/img/sneakers2.jpg"
-    />
-    <Card 
-    title="Женские кроссовки Nike Pink" 
-    price={'9999 ₽'} 
-    imageUrl="/img/sneakers3.jpg"
-    />
-    <Card 
-    title="Мужские кроссовки New Balance 574s" 
-    price={'7999 ₽'} 
-    imageUrl="/img/sneakers4.jpg"
-    />
-   
-  </div>
-</div>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex align-center">
+            <img width={14} height={14} src="/img/search.png" alt="Search"></img>
+            <input placeholder="Поиск"></input>
+          </div>
+        </div>
+        <div className="d-flex">
+          {arr.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
