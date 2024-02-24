373ee3f60a149fd1354fc82cac362ef5655ba4fe
Strokow <aleksestroko@yandex.com>
2024-02-24 16:34:15 +0100

    2402 update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Drawer from './components/Drawer';
 
 function App() {
   const [items, setItems] = useState([]);
+  
+  const [searchValue, setSearchValue] = useState('');
   const [cartOpened, setCartOpened] = useState(false);
 
   useEffect(() => {
@@ -13,24 +15,29 @@ function App() {
       .then((data) => setItems(data));
   }, []);
 
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="wrapper clear">
-      {cartOpened ? <Drawer onClose={() => setCartOpened(false)} /> : null}
+      {cartOpened && <Drawer onClose={() => setCartOpened(false)} />}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ?  `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block d-flex align-center">
             <img width={14} height={14} src="/img/search.png" alt="Search"></img>
-            <input placeholder="Поиск"></input>
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск"></input>
           </div>
         </div>
         <div className="d-flex">
-          {items.map((obj) => (
-            <Card 
-              title={obj.title} 
-              price={obj.price} 
-              imageUrl={obj.imageUrl}
+          {items.map((item, index) => (
+            <Card
+              key={index}
+              title={item.title} 
+              price={item.price} 
+              imageUrl={item.imageUrl}
               onFavorite={() => console.log('Добавили в закладки')}
               onPlus={() => console.log('Нажали плюс')}
             />
