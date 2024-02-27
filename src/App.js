39d8e1532d149e82df4b975c0db29e877094ccc0
Strokow <aleksestroko@yandex.com>
2024-02-27 17:56:02 +0100

    2702 update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'; // Corrected import statement
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
@@ -10,29 +11,33 @@ function App() {
   const [cartOpened, setCartOpened] = useState(false);
 
   useEffect(() => {
-    fetch('https://65d8caaec96fbb24c1bc5059.mockapi.io/Items')
-      .then((response) => response.json())
-      .then((data) => setItems(data));
+    axios.get('https://65d8caaec96fbb24c1bc5059.mockapi.io/Items')
+      .then(res => {
+        setItems(res.data);
+      });
   }, []);
-
+  
   const onAddToCart = (obj) => {
-    setCartItems([...cartItems, obj])
+    axios.post('https://65d8caaec96fbb24c1bc5059.mockapi.io/Cart', obj)
+      .then(res => {
+        setCartItems([...cartItems, obj]);
+      });
   };
-
+  
   const onChangeSearchInput = (event) => {
     setSearchValue(event.target.value);
-  }
-
+  };
+  
   return (
     <div className="wrapper clear">
       {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>{searchValue ?  `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block d-flex align-center">
-            <img width={14} height={14} src="/img/search.png" alt="Search"></img>
-            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск"></input>
+            <img width={14} height={14} src="/img/search.png" alt="Search" />
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск" />
           </div>
         </div>
         <div className="d-flex">
@@ -43,7 +48,7 @@ function App() {
               price={item.price} 
               imageUrl={item.imageUrl}
               onFavorite={() => console.log('Добавили в закладки')}
-              onPlus={(obj) => onAddToCart(obj)}
+              onPlus={() => onAddToCart(item)}
             />
           ))}
         </div>
