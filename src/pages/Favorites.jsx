4d7f98ae5443e5de24b92ac7bb97be215d2fcfb0
Strokow <aleksestroko@yandex.com>
2024-03-12 12:34:12 +0100

    1203 update second

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import Card from '../components/Card';
 
 function Favorites({items, onAddToFavorite}) {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Card from '../components/Card';
-import { useState } from 'react';
+
 
 function Home({
     items: initialItems,
